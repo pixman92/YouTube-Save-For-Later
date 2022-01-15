7ca397ead1e3b0c3d0f3fc1c5225773dc45a3970
pixman92 <GOtrustD@verizon.net>
2022-01-14 23:52:30 -0800

    updating with - hooking HTML to JS

---
+++ DevelopmentOLD/js/firebaseStuff.js
@@ -0,0 +1,30 @@
+window.onload = ()=>{
+  init();
+};
+var db;
+function init(){
+  var firebaseApp = firebase.initializeApp(firebaseConfig);
+
+    db = firebaseApp.firestore();
+}
+
+function pullData(){
+  // your standard - pulling data from Firebase
+  db.collection("YouTube").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+    });
+});
+}
+
+function pushData(data){
+  // data should be JSONstring, for beginning a data push to a new record
+  db.collection("YouTube").add(data).then((docRef) => {
+      console.log("Document written with ID:", docRef.id);
+  })
+  .catch((error) => {
+      console.error("Error adding document: ", error);
+  });
+}
+
+// =================
